8c0aa1bf26c50df903a55863ccb6a915c59cddba
Sachin Rathor <sachinrathor880@gmail.com>
2024-02-06 11:06:23 +0530

    static files setup done

--- mypro/settings.py
+++ mypro/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for mypro project.
 
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom add 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
